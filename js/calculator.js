436bf7726c53c501ed164d833c33e7c1d64f9ea3
zxdinay <deepcut.atm@gmail.com>
2025-04-28 12:46:58 +0800

    Added a function to the js to display the digits upon click

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,40 @@
 /* 
 Author : larakielcode
 Date: April 28, 2025
+
+
+Code starts below
 */
+
+let num1 = "";
+let num2 = 0;
+let operator = "";
+
+function operate(a, b, oper) {
+    return a + oper + b;
+}
+
+function refreshScreen(args) {
+    if (args === '') {
+        document.getElementById("screen").value = '';
+    }
+
+    document.getElementById("screen").value = num1;
+    console.log(num1);
+    console.log(num1.length);
+}
+
+function displayOnScreen() {
+    if (this.textContent === "C") {
+        num1 = "";
+        refreshScreen('');
+    } else {
+        num1 += this.textContent;
+        refreshScreen(num1);
+    }
+
+}
+
+const buttonContainer = document.querySelector(".buttons");
+const buttons = buttonContainer.querySelectorAll("button");
+buttons.forEach(btn => btn.addEventListener("click", displayOnScreen));
